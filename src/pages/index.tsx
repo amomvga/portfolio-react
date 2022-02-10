34642a5d8f1a04858ccdcb1e94b880139c53a7ab
amomvga <amom_augusto@hotmail.com>
2022-02-10 15:58:41 -0300

    contact link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  SimpleGrid,
+  Text,
+  Link,
+} from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import homeImg from "../assets/home-main.svg";
 import Image from "next/image";
+import { RiGithubFill, RiLinkedinBoxFill } from "react-icons/ri";
 
 function Home() {
   return (
-    <Flex direction="column" width="100vw" height="100vh">
+    <Flex direction="column" width="100%" height="100vh">
       <Flex>
         <Header />
       </Flex>
@@ -23,13 +32,36 @@ function Home() {
               Me chamo Amom Augusto
             </Text>
             <Text fontSize={["2xl", "3xl", "xx-large"]}>
-              Desenvolvedor React.js | Node.js
+              Desenvolvedor&#160;
+              <Text
+                as="span"
+                color="pink.500"
+                fontSize={["2xl", "3xl", "xx-large"]}
+              >
+                React.js | Node.js
+              </Text>
             </Text>
           </Box>
           <Box>
             <Image src={homeImg} />
           </Box>
         </SimpleGrid>
+      </Flex>
+      <Flex direction="column" align="center">
+        <Flex>
+          <HStack spacing="2">
+            <Link
+              as={RiLinkedinBoxFill}
+              fontSize={["2xl", "4xl"]}
+              _hover={{ color: "purple.700" }}
+            />
+            <Link
+              as={RiGithubFill}
+              fontSize={["2xl", "4xl"]}
+              _hover={{ color: "purple.700" }}
+            />
+          </HStack>
+        </Flex>
       </Flex>
     </Flex>
   );
